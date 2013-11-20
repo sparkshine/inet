ed245d8b89b633265d6cd8401d46c97ef52c8374
Andras Varga <andras@omnetpp.org>
2013-11-20 11:25:46 +0100

    tmp

--- src/networklayer/common/INetworkProtocolControlInfo.h
+++ src/networklayer/common/INetworkProtocolControlInfo.h
@@ -15,40 +15,69 @@
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
-#ifndef INETWORKPROTOCOLCONTROLINFO_H_
-#define INETWORKPROTOCOLCONTROLINFO_H_
+#ifndef INET_INETWORKPROTOCOLCONTROLINFO_H_
+#define INET_INETWORKPROTOCOLCONTROLINFO_H_
 
 #include "Address.h"
 #include "INetworkDatagram.h"
 
-class INET_API INetworkProtocolControlInfo {
+
+/**
+ * Abstract interface for network-layer (L3) control info classes. This control info
+ * is used for sending packets over a network protocol from higher layers,
+ * and by the network protocol to report properties of a received datagram
+ * to higher layers.
+ */
+class INET_API INetworkProtocolControlInfo
+{
   public:
     virtual ~INetworkProtocolControlInfo() { }
 
+    /**
+     * Identifies the encapsulated protocol, using IANA IPv4/IPv6 Protocol Numbers.
+     * If the network layer protocol uses a different way of id
+     */
     virtual short getProtocol() const = 0;
     virtual void setProtocol(short protocol) = 0;
 
+    /**
+     * Source address of the datagram.
+     */
     virtual Address getSourceAddress() const = 0;
     virtual void setSourceAddress(const Address & address) = 0;
 
+    /**
+     * Destination address of the datagram.
+     */
     virtual Address getDestinationAddress() const = 0;
     virtual void setDestinationAddress(const Address & address) = 0;
 
+    /**
+     * Identifies the interface on which the datagram was received, or
+     * should be sent (see InterfaceTable).
+     */
     virtual int getInterfaceId() const = 0;
     virtual void setInterfaceId(int interfaceId) = 0;
 
+    /**
+     * Hop limit for a datagram to be sent, or remaining hops of
+     * a datagram received. Maps to the IPv4 TTL and IPv6 hopLimit
+     * fields. Ignored by network protocols that don't use hop count.
+     */
     virtual short getHopLimit() const = 0;
     virtual void setHopLimit(short hopLimit) = 0;
 
+    /**
+     * If the destination address is multicast and the output interface
+     * is also part of that multicast group, this flag determines whether
+     * local applications should receive the datagram (loop=true) or not
+     * (loop=false). Undefined when receiving a datagram.
+     */
     virtual bool getMulticastLoop() const = 0;
     virtual void setMulticastLoop(bool multicastLoop) = 0;
 
-    virtual unsigned char getTrafficClass() const = 0;
-    virtual void setTrafficClass(unsigned char trafficClass) = 0;
-
-    virtual void setOrigNetworkDatagram(INetworkDatagram *d) = 0;
-    virtual const INetworkDatagram *getOrigNetworkDatagram() const = 0;
-    virtual INetworkDatagram *removeOrigNetworkDatagram() = 0;
+//    virtual unsigned char getTrafficClass() const = 0;
+//    virtual void setTrafficClass(unsigned char trafficClass) = 0;
 };
 
 #endif /* INETWORKPROTOCOLCONTROLINFO_H_ */
